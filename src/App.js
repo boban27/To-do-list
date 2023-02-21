b1e0f54bf3991bb2108d906ed94fa58b5b84e93e
unknown <boki.tanaskovic27@gmail.com>
2023-02-21 22:11:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ToDoForm from './componets/ToDoForm'
+import './App.css'
+import ToDoList from './componets/ToDoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+  
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
